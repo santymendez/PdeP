af6e089d47dd5c94441798aca86692888cbc718b
santymendez <samendez@frba.utn.edu.ar>
2023-06-10 13:43:19 -0300

    update funcional/readme.md

--- funcional/parciales/src/Spec.hs
+++
@@ -1,9 +0,0 @@
-module Spec where
-import PdePreludat
-import Test.Hspec
-
-correrTests :: IO ()
-correrTests = hspec $ do
-    describe "prueba" $ do
-        it "prueba" $ do
-            2 * 2 `shouldBe` 4
